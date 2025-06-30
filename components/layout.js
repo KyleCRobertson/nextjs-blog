cdc800e8f886ca6902a0e8e88074b3cdcd64441f
Kyle Robertson <91642555+KyleCRobertson@users.noreply.github.com>
2025-06-30 13:56:26 -0400

    siteTitle name update

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.scss';
 import Link from 'next/link';
 
 const name = 'Kyle Robertson';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Kyle NextJS Blog';
 
 export default function Layout({ children, home }){
     return (
